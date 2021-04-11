d9e67a51efad68d054cb4258ca19330e2d64ab35
wsbfdzz <49947867+wsbfdzz@users.noreply.github.com>
2021-04-11 20:42:34 +0800

    Add files via upload

---
+++ Back-end/admin.py
@@ -0,0 +1,13 @@
+from app import create_app
+from app.models.base import db
+from app.models.user import User
+
+app = create_app()
+with app.app_context():
+    with db.auto_commit():
+        # 创建一个超级管理员
+        user = User()
+        user.username = 'admin'
+        user.password = '19990814'
+        user.auth = 2
+        db.session.add(user)
